de1bcba7c46b226b59982b58b85895995ef8271e
Bruno Santos <brunosansp@outlook.com>
2020-12-22 09:07:40 -0300

    modulos e components cursos e curso-detalhe

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
   ],
   imports: [
     BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
